b059d0866503f920d65cc5338139d136679c0ed0
guillaumebdx <guillaumeharari@hotmail.com>
2022-01-25 00:54:54 +0100

    fix double yellow

--- src/Service/ColorManager.php
+++ src/Service/ColorManager.php
@@ -10,49 +10,109 @@ class ColorManager
 
     private $aways = [];
 
+    private $attempt;
+
+    private $expected;
+
+    private $expecteds;
+
+    private $countLetters;
+
+    private $buildAways;
+
+    private $firstCountValids = [];
 
     public function build(string $attempt, string $expected): array
     {
+        $this->attempt = $attempt;
+        $this->expected = $expected;
+        $this->expecteds = str_split($expected);
+        $this->countLetters = array_count_values($this->expecteds);
+        $this->buildAways = $this->buildKeysFromLetters();
+        $this->firstCountValids = $this->buildKeysFromLetters();
+        $this->firstCountValidsLetter();
         $result = [];
-        $valids = [];
-        for ($i=0; $i<strlen($attempt); $i++) {
-            if ($attempt[$i] === $expected[$i]) {
-                $result[] = 'green';
-                $valids[] = $attempt[$i];
-                $this->valids[] = $attempt[$i];
-            } else {
-                $result[] = 'blue';
-                $this->errors[] = $attempt[$i];
-            }
-        }
-        $nbYellowInResult = [];
-        for ($i=0; $i<strlen($attempt); $i++) {
-            if ($attempt[$i] !== $expected[$i] && !in_array($attempt[$i], $valids)) {
-                if (in_array($attempt[$i], str_split($expected))) {
-                    $nbYellowInResult[$attempt[$i]] = $nbYellowInResult[$attempt[$i]] ?? 1;
-                    if ($nbYellowInResult[$attempt[$i]] <= array_count_values(str_split($expected))[$attempt[$i]]) {
-                        $result[$i] = 'yellow';
-                        $this->aways[] = $attempt[$i];
-                        $nbYellowInResult[$attempt[$i]]++;
-                    }
-                }
-            }
+        for ($i = 0; $i < strlen($attempt); $i++) {
+            $result[] = $this->letterToColor($attempt[$i], $i);
         }
         return $result;
     }
 
-    public function getErrors()
+    public function firstCountValidsLetter(): void
     {
-        return $this->errors;
+        for ($i = 0; $i < strlen($this->attempt); $i++) {
+            if ($this->attempt[$i] === $this->expected[$i]) {
+                $this->firstCountValids[$this->attempt[$i]]++;
+            }
+        }
     }
 
-    public function getValids()
+    public function buildKeysFromLetters()
+    {
+        $result = [];
+        foreach ($this->expecteds as $letter) {
+            $result[$letter] = 0;
+        }
+        return $result;
+    }
+
+    public function letterToColor(string $letter, $position)
+    {
+        if ($letter === $this->expected[$position]) {
+            $result = 'green';
+            $this->valids[] = $letter;
+        } elseif (in_array($letter, $this->expecteds)) {
+            $result = $this->checkIfYellowOrBlue($letter);
+        } else {
+            $result = 'blue';
+            $this->errors[] = $letter;
+        }
+        return $result;
+    }
+
+    public function checkIfYellowOrBlue(string $letter)
+    {
+        if ($this->notMoreAwayThanTotal($letter) && $this->notMoreGreenThanTotal($letter)) {
+            $this->buildAways[$letter]++;
+            $this->aways[] = $letter;
+            return 'yellow';
+        } else {
+            $this->errors[] = $letter;
+            return 'blue';
+        }
+    }
+
+    public function notMoreAwayThanTotal($letter)
+    {
+        return $this->buildAways[$letter] < $this->countLetters[$letter];
+    }
+
+    public function notMoreGreenThanTotal($letter)
+    {
+        $result = false;
+        if ($this->firstCountValids[$letter] < $this->buildAways[$letter] + $this->countLetters[$letter]) {
+            $result = true;
+        }
+        return $result;
+    }
+
+    public function hasOneOccurence($letter): bool
+    {
+        return $this->countLetters[$letter] === 1;
+    }
+
+    public function getAways(): array
+    {
+        return $this->aways;
+    }
+
+    public function getValids(): array
     {
         return $this->valids;
     }
 
-    public function getAways()
+    public function getErrors(): array
     {
-        return $this->aways;
+        return $this->errors;
     }
 }
