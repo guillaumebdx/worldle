4c88e53d937fed34d1b769008ef0bb40b5b251f5
pierre-gt <44671639+pierre-gt@users.noreply.github.com>
2022-01-24 19:51:32 +0100

    correction des lettres jaunes
    si la lettre est présente plus d'une fois dans le mot proposé et dans la solution et que

--- src/Service/ColorManager.php
+++ src/Service/ColorManager.php
@@ -15,22 +15,24 @@ class ColorManager
     {
         $result = [];
         $valids = [];
+        $nbYellowInResult = [];
         for ($i=0; $i<strlen($attempt); $i++) {
             if ($attempt[$i] === $expected[$i]) {
                 $result[] = 'green';
                 $valids[] = $attempt[$i];
                 $this->valids[] = $attempt[$i];
+                $nbYellowInResult[$attempt[$i]] = $nbYellowInResult[$attempt[$i]] ?? 0;
+                $nbYellowInResult[$attempt[$i]]++;
             } else {
                 $result[] = 'blue';
                 $this->errors[] = $attempt[$i];
             }
         }
-        $nbYellowInResult = [];
         for ($i=0; $i<strlen($attempt); $i++) {
-            if ($attempt[$i] !== $expected[$i] && !in_array($attempt[$i], $valids)) {
+            if ($attempt[$i] !== $expected[$i]) {
                 if (in_array($attempt[$i], str_split($expected))) {
-                    $nbYellowInResult[$attempt[$i]] = $nbYellowInResult[$attempt[$i]] ?? 1;
-                    if ($nbYellowInResult[$attempt[$i]] <= array_count_values(str_split($expected))[$attempt[$i]]) {
+                   $nbYellowInResult[$attempt[$i]] = $nbYellowInResult[$attempt[$i]] ?? 0;
+                    if ($nbYellowInResult[$attempt[$i]] < array_count_values(str_split($expected))[$attempt[$i]]) {
                         $result[$i] = 'yellow';
                         $this->aways[] = $attempt[$i];
                         $nbYellowInResult[$attempt[$i]]++;
