94e702271309d078eff91d30a27372c774f6b243
guillaumebdx <guillaumeharari@hotmail.com>
2022-02-07 23:59:41 +0100

    add paginator

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Attempt;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -22,13 +24,15 @@ class AdminController extends AbstractController
     /**
      * @Route("/tentatives/{date}", name="attempt")
      */
-    public function attempt($date = null)
+    public function attempt(PaginatorInterface $paginator, Request $request, $date = null)
     {
         $date = $date ? new \DateTime($date) : new \DateTime();
         $attempts = $this->managerRegistry
             ->getRepository(Attempt::class)
             ->findBy(['createdAt' => $date], ['id' => 'DESC']);
+        $pagination = $paginator->paginate($attempts, $request->query->getInt('page', 1),100);
         return $this->render('admin/attempt.html.twig', [
+            'pagination' => $pagination,
             'attempts' => $attempts,
             'date'     => $date,
         ]);
