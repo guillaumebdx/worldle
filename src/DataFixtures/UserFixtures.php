81c062f20c4b05c72e71fa31c02bc63f118d8656
cegaz <cecilegaz@gmail.com>
2025-03-10 13:53:25 +0100

    composer update & update UserFixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -16,7 +16,7 @@ class UserFixtures extends Fixture
         $this->hasher = $hasher;
     }
 
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         $user = new User();
         $user->setUsername('guillaume');
